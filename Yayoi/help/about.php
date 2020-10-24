66ed69fba82b70d5922b114e591633d407c3ec70
earndiva <56571158+earndiva@users.noreply.github.com>
2020-10-25 03:04:38 +0700

    Update about+condition.php

--- Yayoi/help/about.php
+++ Yayoi/help/about.php
@@ -3,9 +3,173 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/notoFont.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://yayoirestaurants.com/frontend/assets/dist/app.css?v=2" type="text/css">
+    <link rel="stylesheet" href="https://yayoirestaurants.com/frontend/assets/dist/aboutus.css" type="text/css">
+    <script src="../js/scripts.js"></script>
+    <script src="https://kit.fontawesome.com/a0ce56af97.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+
+    <link rel="shortcut icon" href="https://yayoirestaurants.com/frontend/assets/images/favicon.ico" type="image/x-icon" >
+    <title>เกี่ยวกับยาโยอิ Yayoi Restaurant : Modern Japanese Restaurant in Thailand - ยาโยอิ เซ็ตอร่อยของทุกคน</title>
+    <style>
+        .item_conditions { font-size: 14px; line-height: 18px; margin-top: 20px;}
+        .promo_note { font-size: 14px; color: #eb7aae}
+        strong { color: #eb7aae}
+    </style>
 </head>
 <body>
-    Hi เกี่ยวกับยาโยอิ
+<header class="notoFont">
+        <div class="topheader shadow">
+            <div class="container notoFont">
+                <div class="row p-3">
+                    <div class="col-md-4">
+                        <div class="row justify-content-start">
+                            <div class="col-md-8">
+                                <a href="../index.php"><div class="toppic p-3 shadow-lg logoYayoi" style="z-index: 1;">
+                                    <img src="https://yayoirestaurants.com/frontend/assets/images/logo.svg" alt="Yayoi Restaurant - เสิร์ฟร้อน อร่อยเร็ว" width="100%">
+                                </div></a>
+                            </div>
+                            <div class="col-md-4 font-weight-bold"><a class="headMenu" href="promoFood.php" class="float-right">เมนูยาโยอิ</a></div>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="row justify-content-end">
+                            <div class="headMenuBox text-center">
+                                <a class="headMenu" href="">เข้าระบบ</a>
+                            </div> |
+                            <div class="headMenuBox">
+                                <a class="headMenu" href="">สมัครสมาชิก</a>
+                            </div>|
+                            <div class="headMenuBox">
+                                <div class="dropdown">
+                                    <button class="dropbtn">
+                                        TH
+                                        <span style="font-size: 20px; color: white;">
+                                            <i class="fas fa-angle-down"></i>
+                                        </span>
+                                    </button>
+                                    <div class="dropdown-content">
+                                    <a href="#">EN</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="headMenuBox" onmouseover="changeIconDer('over')" onmouseout="changeIconDer('out')">
+                                <button class="btn font-weight-bold orderFoodBt"><img id='deliveryIcon' src="../icon/delivery.PNG" height="21px"> สั่งอาหารทันที</button>
+                            </div>
+                            <div class="headMenuBox" onmouseover="changeIconBasket('over')" onmouseout="changeIconBasket('out')">
+                                <a class="headMenu" href=""><img id='BasketIcon' src="../icon/busket.PNG" width="30px"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    <section>
+        <div class="container py-5">
+            <h1 class="ci-primary text-center mb-4 mb-1g-5">
+                <strong>เกี่ยวกับยาโยอิ</strong>
+            </h1>
+        
+        <div class="row gutter-30 mb-4 mb-lg-5">
+            <section class="col-lg-6 col-12 text-center order-lg-2 mb-4 mb-lg-0">
+                <img src="https://yayoirestaurants.com/frontend/assets/images/aboutus/about_cover_01.jpg" width="100%" alt="ตำนาน ยาโยอิ">
+            </section>
+            <section class="col-lg-6 col-12 text-center text-lg-left order-lg-1">
+                    <h2 class="ci-primary mb-3">
+                        ตำนาน ยาโยอิ
+                    </h2>
+            
+
+            <p>ยุคสมัยเมจิในญี่ปุ่น (ประมาณ 125 ปีที่แล้ว) เป็นช่วงที่ความเจริญของตะวันตก 
+            ได้หลั่งไหลเข้าสู่ประเทศญี่ปุ่น ซึ่งในสมัยนั้น คุณ&nbsp;<strong>&ldquo;ทามิจิโร ชิโออิ&rdquo;</strong>&nbsp;
+            ได้กลับจากการศึกษาการเป็นเชฟ จากฝรั่งเศสและได้ริเริ่มเปิดร้าน&nbsp;<strong>&ldquo;ยาโยอิ&rdquo;</strong>&nbsp;สาขาแรกที่โตเกียวในบริเวณที่เรียกว่าคายาบาโจ สาขา แรกนี้เสิร์ฟเมนูตะวันตก เพียงเซ็ตละ 1 เยนเท่านั้น 
+            และด้วยฝีมือปรุงอาหารซึ่งพิถีพิถันทำให้ร้าน&nbsp;<strong>&ldquo;ยาโยอิ&rdquo;</strong>&nbsp;เป็นที่นิยมของชาวโตเกียว 
+            มีทั้งนักเขียนชื่อดังจุนอิชิโร ทานิซากิ และนายกรัฐมนตรีในสมัยนั้นก็เป็นลูกค้าประจำ ต่อจากนั้นมาหลานชายของคุณทามิจิโร คือ คุณ&nbsp;<strong>&ldquo;สุเอยูกิ ชิโออิ&rdquo;</strong>&nbsp;
+            ได้ดำเนินกิจการของคุณปู่ในเวลาต่อมาจนปัจจุบันมีสาขากว่า 200 สาขา และจะเปิดครบ 500 สาขาในอนาคต
+            </p>
+            <p>เมนูที่ขึ้นชื่อและเป็นที่นิยมในปัจจุบันจะผสมผสานความวัฒนธรรมตะวันตก และต้นตำรับอาหารญี่ปุ่นเข้าด้วยกันโดยยังคงรักษาความอร่อยของอาหาร จากฝีมือที่เป็นเลิศเสมอมา
+            </p>
+            </section>
+        </div>
+
+        <div class="row gutter-30 align-items-center mb-4 mb-lg-5">
+                <section class="col-lg-6 col-12 text-center order-lg-1 mb-4 mb-lg-0">
+                    <img src="https://yayoirestaurants.com/frontend/assets/images/aboutus/about_cover_02.jpg" width="100%" alt="ตำนาน ยาโยอิ">
+                </section>
+                <section class="col-lg-6 col-12 text-center text-lg-left order-lg-2">
+                    <p>
+                        นอกจาก ร้าน <strong>“ยาโยอิ”</strong> แล้วคุณสุเอยูกิยังดำเนินธุรกิจเครือข่ายร้านอาหาร ข้าวกล่อง (เบ็นโตะ) ทั่วประเทศญี่ปุ่น กว่า 2,400 สาขา ซึ่งประสบความสำเร็จเป็นอย่างมากและบริษัทได้จดทะเบียนอยู่ในตลาดหลักทรัพย์ประเทศญี่ปุ่น ชาวญี่ปุ่นรู้จักกันดีในชื่อว่า <strong>“Hotto Motto”</strong>
+                    </p>
+                    <p>
+                        <a href="https://kome-academy.com/en/" target="_blank" class="ci-primary">ศูนย์วิจัยและพัฒนาอาหารญี่ปุ่นโดย บ.เพลนัส</a>
+                        <br />
+                        <a href="https://www.yayoiken.com/en/" target="_blank" class="ci-primary">เว็บไซต์ยาโยอิ ญี่ปุ่น</a>
+                    </p>
+                </section>
+            </div>
+
+            <div class="row gutter-60 align-items-center">
+                <section class="col-lg-12 col-12 text-center order-lg-2 mb-4 mb-lg-0">
+                    <img src="https://yayoirestaurants.com/frontend/assets/images/aboutus/about_cover_03.jpg" alt="ปณิธาน">
+                </section>
+                <section class="col-lg-12 col-12 order-lg-1">
+                    <h2 class="ci-primary text-center mb-3">
+                        ปณิธาน
+                    </h2>
+                    <p class="text-center">
+                        ยาโยอิเป็นร้านอาหารจากประเทศญี่ปุ่น ที่เสิร์ฟอาหารจานร้อน และรสชาติสไตล์ต้นตำรับ ด้วยราคาที่คุ้มค่าโดยมีหลากหลายของเมนูให้ทุกคนได้เลือกสรร<br class="d-none d-lg-block">
+                        ไม่ใช่เพียงเท่านี้ยาโยอิยังคัดสรรวัตถุดิบคุณภาพดีเพื่อมาทำอาหาร และรับประกันในรสชาติ ที่เอร็ดอร่อยของเราว่าจะโดนใจทุกคน ที่ได้แวะมาใช้บริการยาโยอิ
+                    </p>
+                </section>
+            </div>
+
+        </div>
+    </section>
+
+    </header>
+
+
+    <footer class="text-center">
+        <div class="container">
+            <a href="../index.php">
+                <img width="200px" src="https://yayoirestaurants.com/frontend/assets/images/logo.svg" alt="Yayoi Restaurant - เสิร์ฟร้อน อร่อยเร็ว">
+            </a><br><br>
+            <div id="aniDelivery">
+                <img width="79px" src="https://yayoirestaurants.com/frontend/assets/images/icon/delivery_icon.svg">
+            </div>
+            <div id="footerMenu">
+                <div class="FMenu"><a href="promo.php">โปรโมชั่น</a></div>
+                <div class="FMenu"><a href="branch.php">สาขายาโยอิ</a></div>
+                <div class="FMenu"><a href="about.php">เกี่ยวกับยาโยอิ</a></div>
+                <div class="FMenu"><a href="contact.php">ติดต่อเรา</a></div>
+                <div class="FMenu"><a href="sugguest.php">แนะนำติชมการให้บริการ</a></div>
+                <div class="FMenu"><a href="condition.php">เงื่อนไขการให้บริการ</a></div>
+                <div class="FMenu"><a href="policy.php">นโยบายความเป็นส่วนตัว</a></div>
+            </div><br>
+            <div id="contact">
+                <b>ติดตามเราที่นี่</b><br><br>
+                <div><a href="https://www.facebook.com/YayoiRestaurantThailand" target="_blank"><img src="https://yayoirestaurants.com/frontend/assets/images/icon/fb_icon.svg"></a></div>
+                <div><a href="https://line.me/R/ti/p/%40327rcyjo" target="_blank"><img src="https://yayoirestaurants.com/frontend/assets/images/icon/line_icon.svg"></a></div>
+                <div><a href="https://www.instagram.com/yayoith_official/?igshid=p9ki0k2ayxv1" target="_blank"><img src="https://yayoirestaurants.com/frontend/assets/images/icon/ins_icon.svg"></a></div>
+                <div><a href="https://www.youtube.com/user/YayoiThailand" target="_blank"><img src="https://yayoirestaurants.com/frontend/assets/images/icon/yt_icon.svg"></a></div>
+            </div>
+            
+        </div><br>
+        <div id="credit">
+            <div style="transform: translateY(10px);">
+                <img width="100%" src="https://yayoirestaurants.com/frontend/assets/images/bg_footer_bottom.png?c3d6b62">
+            </div>
+            <div id="creditD">© 2557-2563 บริษัท เอ็มเค อินเตอร์ฟู้ด</div>
+        </div>
+    </footer>
+
+
 </body>
 </html>
