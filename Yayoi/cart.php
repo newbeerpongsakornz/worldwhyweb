e5a9eace8d5c1267e84cba8cb8a9cda1ec6c8e13
gxmmmm <62070175@it.kmitl.ac.th>
2020-11-01 15:40:05 +0700

    add cart

--- Yayoi/cart.php
+++ Yayoi/cart.php
@@ -3,9 +3,114 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/notoFont.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="../js/scripts.js"></script>
+    <script src="https://kit.fontawesome.com/a0ce56af97.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+    <link rel="shortcut icon" href="https://yayoirestaurants.com/frontend/assets/images/favicon.ico" type="image/x-icon" >
+    <link href="http://mozilla.github.io/foundation-icons/assets/foundation-icons.css" type="text/css" rel="stylesheet">
+    
+    <title>Yayoi Restaurant : Modern Japanese Restaurant in Thailand - ยาโยอิ เซ็ตอร่อยของทุกคน</title>
 </head>
-<body>
-    Hi รถเข็น
+<body class="notoFont">
+
+<header>
+        <div class="topheader shadow">
+            <div class="container notoFont">
+                <div class="row p-3">
+                    <div class="col-md-5">
+                        <div class="row justify-content-start">
+                            <div class="col-md-7">
+                                <a href="../index.php"><div class="toppic p-3 shadow-lg logoYayoi" style="z-index: 1;">
+                                    <img src="https://yayoirestaurants.com/frontend/assets/images/logo.svg" alt="Yayoi Restaurant - เสิร์ฟร้อน อร่อยเร็ว" width="100%">
+                                </div></a>
+                            </div>
+                            <div class="col-md-5 font-weight-bold pt-2 "><a class="headMenu" href="../menu/promoFood.php" class="float-right">เมนูยาโยอิ</a></div>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="row justify-content-end">
+                            <div class="headMenuBox pt-2">
+                                <a class="headMenu" href="../login.php">เข้าระบบ</a>
+                                <b class="pl-2">|</b></div> 
+                            <div class="headMenuBox pt-2 pl-0 pr-1">
+                                <a class="headMenu" href="../register.php">สมัครสมาชิก</a>
+                                <b class="pl-2">|</b></div>
+                            <div class="headMenuBox pt-1">
+                                <div class="dropdown">
+                                    <button class="dropbtn">
+                                        TH
+                                        <span style="font-size: 20px; color: white;">
+                                            <i class="fas fa-angle-down"></i>
+                                        </span>
+                                    </button>
+                                    <div class="dropdown-content">
+                                    <a href="#">EN</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="headMenuBox" onmouseover="changeIconDer('over')" onmouseout="changeIconDer('out')">
+                                <button onclick="window.location.href='promoFood.php'" class="btn font-weight-bold orderFoodBt"><img id='deliveryIcon' src="../icon/delivery.PNG" height="21px"> สั่งอาหารทันที</button>
+                            </div>
+                            <div class="headMenuBox pt-1" onmouseover="changeIconBasket('over')" onmouseout="changeIconBasket('out')">
+                                <a class="headMenu" href="../cart.php"><img id='BasketIcon' src="../icon/busket.PNG" width="30px"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <img src="https://yayoirestaurants.com/mainbanners/3880_Cart.jpg" width="100%">
+    <br><br>
+    <div class="container-fluid ">
+        <div class="border m-3 mb-4">
+            <div class="p-3"><h5><img src="svg.svg" alt="" height="20px" class="svg mr-2"><b>ตรวจสอบรายการอาหาร</b> <img src="up-arrow.svg" alt="" height="20px" class="float-right m-2"> </h5></div>
+            <div class="border-top text-center p-4"><h5 class="mt-3"><b>ไม่พบสินค้าในตะกร้า</b></h5>
+                <div> <button onclick="window.location.href='menu/promoFood.php'" class="buymoreBt rounded p-2 m-2"><b>กลับไปสั่งอาหารเพิ่ม</b></button></div></div>
+            </div>
+            <br>
+    </div>
+       
+        
+
+    <footer class="text-center">
+        <div class="container">
+            <a href="../index.php">
+                <img width="200px" src="https://yayoirestaurants.com/frontend/assets/images/logo.svg" alt="Yayoi Restaurant - เสิร์ฟร้อน อร่อยเร็ว">
+            </a><br><br>
+            <div id="aniDelivery">
+                <img width="79px" src="https://yayoirestaurants.com/frontend/assets/images/icon/delivery_icon.svg">
+            </div>
+            <div id="footerMenu">
+                <div class="FMenu"><a href="../help/promo.php">โปรโมชั่น</a></div>
+                <div class="FMenu"><a href="../help/branch.php">สาขายาโยอิ</a></div>
+                <div class="FMenu"><a href="../help/about.php">เกี่ยวกับยาโยอิ</a></div>
+                <div class="FMenu"><a href="../help/contact.php">ติดต่อเรา</a></div>
+                <div class="FMenu"><a href="../help/sugguest.php">แนะนำติชมการให้บริการ</a></div>
+                <div class="FMenu"><a href="../help/condition.php">เงื่อนไขการให้บริการ</a></div>
+                <div class="FMenu"><a href="../help/policy.php">นโยบายความเป็นส่วนตัว</a></div>
+            </div><br>
+            <div id="contact">
+                <b>ติดตามเราที่นี่</b><br><br>
+                <div><a href="https://www.facebook.com/YayoiRestaurantThailand" target="_blank"><img src="https://yayoirestaurants.com/frontend/assets/images/icon/fb_icon.svg"></a></div>
+                <div><a href="https://line.me/R/ti/p/%40327rcyjo" target="_blank"><img src="https://yayoirestaurants.com/frontend/assets/images/icon/line_icon.svg"></a></div>
+                <div><a href="https://www.instagram.com/yayoith_official/?igshid=p9ki0k2ayxv1" target="_blank"><img src="https://yayoirestaurants.com/frontend/assets/images/icon/ins_icon.svg"></a></div>
+                <div><a href="https://www.youtube.com/user/YayoiThailand" target="_blank"><img src="https://yayoirestaurants.com/frontend/assets/images/icon/yt_icon.svg"></a></div>
+            </div>
+            
+        </div><br>
+        <div id="credit">
+            <div style="transform: translateY(10px);">
+                <img  width="100%" src="https://yayoirestaurants.com/frontend/assets/images/bg_footer_bottom.png?c3d6b62">
+            </div>
+            <div id="creditD">© 2557-2563 บริษัท เอ็มเค อินเตอร์ฟู้ด</div>
+        </div>
+    </footer>
 </body>
 </html>
