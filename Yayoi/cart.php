02e0c9927e231a09497efc01aff02d8cddb44f84
gxmmmm <62070175@it.kmitl.ac.th>
2020-11-01 21:09:56 +0700

    ...

--- Yayoi/cart.php
+++ Yayoi/cart.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/notoFont.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="../js/scripts.js"></script>
+    <script src="js/scripts.js"></script>
     <script src="https://kit.fontawesome.com/a0ce56af97.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
@@ -26,20 +26,20 @@
                     <div class="col-md-5">
                         <div class="row justify-content-start">
                             <div class="col-md-7">
-                                <a href="../index.php"><div class="toppic p-3 shadow-lg logoYayoi" style="z-index: 1;">
+                                <a href="index.php"><div class="toppic p-3 shadow-lg logoYayoi" style="z-index: 1;">
                                     <img src="https://yayoirestaurants.com/frontend/assets/images/logo.svg" alt="Yayoi Restaurant - เสิร์ฟร้อน อร่อยเร็ว" width="100%">
                                 </div></a>
                             </div>
-                            <div class="col-md-5 font-weight-bold pt-2 "><a class="headMenu" href="../menu/promoFood.php" class="float-right">เมนูยาโยอิ</a></div>
+                            <div class="col-md-5 font-weight-bold pt-2 "><a class="headMenu" href="menu/promoFood.php" class="float-right">เมนูยาโยอิ</a></div>
                         </div>
                     </div>
                     <div class="col-md-7">
                         <div class="row justify-content-end">
                             <div class="headMenuBox pt-2">
-                                <a class="headMenu" href="../login.php">เข้าระบบ</a>
+                                <a class="headMenu" href="login.php">เข้าระบบ</a>
                                 <b class="pl-2">|</b></div> 
                             <div class="headMenuBox pt-2 pl-0 pr-1">
-                                <a class="headMenu" href="../register.php">สมัครสมาชิก</a>
+                                <a class="headMenu" href="register.php">สมัครสมาชิก</a>
                                 <b class="pl-2">|</b></div>
                             <div class="headMenuBox pt-1">
                                 <div class="dropdown">
@@ -55,10 +55,10 @@
                                 </div>
                             </div>
                             <div class="headMenuBox" onmouseover="changeIconDer('over')" onmouseout="changeIconDer('out')">
-                                <button onclick="window.location.href='promoFood.php'" class="btn font-weight-bold orderFoodBt"><img id='deliveryIcon' src="../icon/delivery.PNG" height="21px"> สั่งอาหารทันที</button>
+                                <button onclick="window.location.href='promoFood.php'" class="btn font-weight-bold orderFoodBt"><img id='deliveryIcon' src="icon/delivery.PNG" height="21px"> สั่งอาหารทันที</button>
                             </div>
                             <div class="headMenuBox pt-1" onmouseover="changeIconBasket('over')" onmouseout="changeIconBasket('out')">
-                                <a class="headMenu" href="../cart.php"><img id='BasketIcon' src="../icon/busket.PNG" width="30px"></a>
+                                <a class="headMenu" href="cart.php"><img id='BasketIcon' src="icon/busket.PNG" width="30px"></a>
                             </div>
                         </div>
                     </div>
@@ -81,20 +81,20 @@
 
     <footer class="text-center">
         <div class="container">
-            <a href="../index.php">
+            <a href="index.php">
                 <img width="200px" src="https://yayoirestaurants.com/frontend/assets/images/logo.svg" alt="Yayoi Restaurant - เสิร์ฟร้อน อร่อยเร็ว">
             </a><br><br>
             <div id="aniDelivery">
                 <img width="79px" src="https://yayoirestaurants.com/frontend/assets/images/icon/delivery_icon.svg">
             </div>
             <div id="footerMenu">
-                <div class="FMenu"><a href="../help/promo.php">โปรโมชั่น</a></div>
-                <div class="FMenu"><a href="../help/branch.php">สาขายาโยอิ</a></div>
-                <div class="FMenu"><a href="../help/about.php">เกี่ยวกับยาโยอิ</a></div>
-                <div class="FMenu"><a href="../help/contact.php">ติดต่อเรา</a></div>
-                <div class="FMenu"><a href="../help/sugguest.php">แนะนำติชมการให้บริการ</a></div>
-                <div class="FMenu"><a href="../help/condition.php">เงื่อนไขการให้บริการ</a></div>
-                <div class="FMenu"><a href="../help/policy.php">นโยบายความเป็นส่วนตัว</a></div>
+                <div class="FMenu"><a href="help/promo.php">โปรโมชั่น</a></div>
+                <div class="FMenu"><a href="help/branch.php">สาขายาโยอิ</a></div>
+                <div class="FMenu"><a href="help/about.php">เกี่ยวกับยาโยอิ</a></div>
+                <div class="FMenu"><a href="help/contact.php">ติดต่อเรา</a></div>
+                <div class="FMenu"><a href="help/sugguest.php">แนะนำติชมการให้บริการ</a></div>
+                <div class="FMenu"><a href="help/condition.php">เงื่อนไขการให้บริการ</a></div>
+                <div class="FMenu"><a href="help/policy.php">นโยบายความเป็นส่วนตัว</a></div>
             </div><br>
             <div id="contact">
                 <b>ติดตามเราที่นี่</b><br><br>
