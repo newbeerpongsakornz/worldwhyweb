3711bf08785d9b53ac187c7720904b8d555ebe86
nanad21531 <59883103+nanad21531@users.noreply.github.com>
2020-11-03 02:31:28 +0700

    Update contact.php

--- Yayoi/help/contact.php
+++ Yayoi/help/contact.php
@@ -3,9 +3,216 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="_token" content="ww8RQdcHnjUWqw4I4gtQy1AWPZRu3YZaenqjFgFz">
+    <meta name="keywords" content="ยาโยอิ, อาหารญี่ปุ่น, ปลาแซลมอน, กระทะร้อน, ราเม็ง, เบนโตะ, สั่งอาหาร, delivery">
+    <meta name="description" content="ยาโยอิเป็นร้านอาหารจากประเทศญี่ปุ่น ที่เสิร์ฟอาหารจานร้อน และรสชาติสไตล์ต้นตำรับ ด้วยราคาที่คุ้มค่า และคัดสรรวัตถุดิบคุณภาพดีเพื่อมาทำอาหาร รับประกันรสชาติ พร้อมช่องทางใหม่กับความสุขที่พร้อมส">
+    <meta name="robots" content="index,follow" />
+    <meta name="googlebot" content="index,follow" />
+    <meta http-equiv="Content-Language" content="th-TH">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="cache-control" content="max-age=31557600" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="เงื่อนไขการให้บริการ Yayoi Restaurant : Modern Japanese Restaurant in Thailand - ยาโยอิ เซ็ตอร่อยของทุกคน" />
+    <meta property="og:url" content="https://yayoirestaurants.com/terms" />
+    <meta property="og:site_name" content="Yayoi Restaurant : Modern Japanese Restaurant in Thailand - ยาโยอิ เซ็ตอร่อยของทุกคน" />
+    <meta property="og:image" content="https://yayoirestaurants.com/frontend/assets/images/logo.svg" />
+    <meta property="fb:app_id" content="542539216565446"/>
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/notoFont.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://yayoirestaurants.com/frontend/assets/dist/app.css?v=2" type="text/css">
+    <link rel="stylesheet" href="https://yayoirestaurants.com/frontend/assets/dist/term.css" type="text/css">
+    <script src="../js/scripts.js"></script>
+    <script src="https://kit.fontawesome.com/a0ce56af97.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+    <title>ติดต่อเรา Yayoi Restaurant : Modern Japanese Restaurant in Thailand - ยาโยอิ เซ็ตอร่อยของทุกคน</title>
+
+    <base href="/">
+
+    <link rel="shortcut icon" href="https://yayoirestaurants.com/frontend/assets/images/favicon.ico" type="image/x-icon" >
+    
+
+    <link rel="apple-touch-icon" href="https://yayoirestaurants.com/frontend/assets/images/favicon/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="https://yayoirestaurants.com/frontend/assets/images/favicon/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="https://yayoirestaurants.com/frontend/assets/images/favicon/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="https://yayoirestaurants.com/frontend/assets/images/favicon/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="https://yayoirestaurants.com/frontend/assets/images/favicon/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="https://yayoirestaurants.com/frontend/assets/images/favicon/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="https://yayoirestaurants.com/frontend/assets/images/favicon/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="https://yayoirestaurants.com/frontend/assets/images/favicon/apple-touch-icon-152x152.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="https://yayoirestaurants.com/frontend/assets/images/favicon/apple-touch-icon-180x180.png" />
+
+    <style>
+        .item_conditions { font-size: 14px; line-height: 18px; margin-top: 20px;}
+        .promo_note { font-size: 14px; color: #eb7aae}
+
+    </style>
 </head>
 <body>
-    Hi ติดต่อเรา
+<header class="notoFont">
+        <div class="topheader shadow">
+            <div class="container notoFont">
+                <div class="row p-3">
+                    <div class="col-md-4">
+                        <div class="row justify-content-start">
+                            <div class="col-md-8">
+                                <a href="../index.php"><div class="toppic p-3 shadow-lg logoYayoi" style="z-index: 1;">
+                                    <img src="https://yayoirestaurants.com/frontend/assets/images/logo.svg" alt="Yayoi Restaurant - เสิร์ฟร้อน อร่อยเร็ว" width="100%">
+                                </div></a>
+                            </div>
+                            <div class="col-md-4 font-weight-bold"><a class="headMenu" href="promoFood.php" class="float-right">เมนูยาโยอิ</a></div>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="row justify-content-end">
+                            <div class="headMenuBox text-center">
+                                <a class="headMenu" href="">เข้าระบบ</a>
+                            </div> |
+                            <div class="headMenuBox">
+                                <a class="headMenu" href="">สมัครสมาชิก</a>
+                            </div>|
+                            <div class="headMenuBox">
+                                <div class="dropdown">
+                                    <button class="dropbtn">
+                                        TH
+                                        <span style="font-size: 20px; color: white;">
+                                            <i class="fas fa-angle-down"></i>
+                                        </span>
+                                    </button>
+                                    <div class="dropdown-content">
+                                    <a href="#">EN</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="headMenuBox" onmouseover="changeIconDer('over')" onmouseout="changeIconDer('out')">
+                                <button class="btn font-weight-bold orderFoodBt"><img id='deliveryIcon' src="../icon/delivery.PNG" height="21px"> สั่งอาหารทันที</button>
+                            </div>
+                            <div class="headMenuBox" onmouseover="changeIconBasket('over')" onmouseout="changeIconBasket('out')">
+                                <a class="headMenu" href=""><img id='BasketIcon' src="../icon/busket.PNG" width="30px"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </header>
+    
+
+    <!-- main -->
+    <img src="https://yayoirestaurants.com/mainbanners/9982_Contact.jpg" width="100%">
+    <br><br>
+    <div class="text-center container">
+        <h1><b class="headBody">ติดต่อเรา</b></h1>
+    </div>
+    <br>
+    <div class="container">
+    <div class="row justify-content-center h-100">
+        <div class="col-5" id="register">
+            <label><b>ชื่อ - นามสกุล</b><b class="redColor">*</b></label>
+            <input type="text" id="fullname" name="fullname" class="form-control">
+            <label><b>อีเมล</b><b class="redColor">*</b></label>
+            <input type="text" id="email" name="email" class="form-control">
+            <label><b>เบอร์โทรศัพท์มือถือ</b><b class="redColor">*</b></label>
+            <input type="text" id="tel" name="tel" class="form-control">
+            <label><b>เรื่องที่ต้องการติดต่อ</b><b class="redColor">*</b></label>
+            <input type="text" id="subject" name="subject" class="form-control">
+            <label><b>ข้อความที่ต้องการติดต่อ</b><b class="redColor">*</b></label>
+            <textarea type="text" id="message" name="message" rows="5" class="form-control"></textarea>
+            <br>
+            <button class="btn btn-lg btn-round btn-primary ladda-button">ส่งข้อความ</button>
+            <br><br>
+        </div>
+        <div class="col-5" style="padding:20px; background:#fce9f2; border-radius:10px">
+            <div class="row justify-content-center align-items-center h-100">
+            <div class="col-auto">
+                <h5><b>บริษัท เอ็มเค อินเตอร์ฟู้ด จำกัด</b></h5>
+                <p class="ic-indent mb-2" data-indent="40">
+                    <i class="h3 ci-primary" data-top="12">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-geo-alt" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M12.166 8.94C12.696 7.867 13 6.862 13 6A5 5 0 0 0 3 6c0 .862.305 1.867.834 2.94.524 1.062 1.234 2.12 1.96 3.07A31.481 31.481 0 0 0 8 14.58l.208-.22a31.493 31.493 0 0 0 1.998-2.35c.726-.95 1.436-2.008 1.96-3.07zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+                            <path fill-rule="evenodd" d="M8 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                        </svg></i>
+                        1200 ถนนเทพรัตน แขวงบางนาใต้<br>
+                        เขตบางนา กรุงเทพ 10260</p>
+                <p class="ic-indent mb-2" data-indent="40">
+                    <i class="h3 ci-primary" data-top="12">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-clock" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm8-7A8 8 0 1 1 0 8a8 8 0 0 1 16 0z"/>
+                            <path fill-rule="evenodd" d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z"/>
+                        </svg></i>
+                        จันทร์-ศุกร์ เวลา 8.00-21.00 น.<br>
+                        เสาร์-อาทิตย์ และวันหยุดนักขัตฤกษ์ เวลา 10.00-21.00 น.</p>
+                <p class="ic-indent mb-2" data-indent="40">
+                    <i class="h3 ci-primary" data-top="0">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-telephone-outbound" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM11 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-4.146 4.147a.5.5 0 0 1-.708-.708L14.293 1H11.5a.5.5 0 0 1-.5-.5z"/>
+                        </svg></i>
+                        02-836-1088</p>
+                <p class="ic-indent mb-2" data-indent="40">
+                    <i class="h3 ci-primary" data-top="0">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-printer" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11 2H5a1 1 0 0 0-1 1v2H3V3a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2h-1V3a1 1 0 0 0-1-1zm3 4H2a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h1v1H2a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1z"/>
+                            <path fill-rule="evenodd" d="M11 9H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1zM5 8a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H5z"/>
+                            <path d="M3 7.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+                        </svg></i>
+                        02-836-1099</p>
+            </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <br><br>
+    <!--main-->
+
+
+        <footer class="text-center">
+        <div class="container">
+            <a href="../index.php">
+                <img width="200px" src="https://yayoirestaurants.com/frontend/assets/images/logo.svg" alt="Yayoi Restaurant - เสิร์ฟร้อน อร่อยเร็ว">
+            </a><br><br>
+            <div id="aniDelivery">
+                <img width="79px" src="https://yayoirestaurants.com/frontend/assets/images/icon/delivery_icon.svg">
+            </div>
+            <div id="footerMenu">
+                <div class="FMenu"><a href="promo.php">โปรโมชั่น</a></div>
+                <div class="FMenu"><a href="branch.php">สาขายาโยอิ</a></div>
+                <div class="FMenu"><a href="about.php">เกี่ยวกับยาโยอิ</a></div>
+                <div class="FMenu"><a href="contact.php">ติดต่อเรา</a></div>
+                <div class="FMenu"><a href="sugguest.php">แนะนำติชมการให้บริการ</a></div>
+                <div class="FMenu"><a href="condition.php">เงื่อนไขการให้บริการ</a></div>
+                <div class="FMenu"><a href="policy.php">นโยบายความเป็นส่วนตัว</a></div>
+            </div><br>
+            <div id="contact">
+                <b>ติดตามเราที่นี่</b><br><br>
+                <div><a href="https://www.facebook.com/YayoiRestaurantThailand" target="_blank"><img src="https://yayoirestaurants.com/frontend/assets/images/icon/fb_icon.svg"></a></div>
+                <div><a href="https://line.me/R/ti/p/%40327rcyjo" target="_blank"><img src="https://yayoirestaurants.com/frontend/assets/images/icon/line_icon.svg"></a></div>
+                <div><a href="https://www.instagram.com/yayoith_official/?igshid=p9ki0k2ayxv1" target="_blank"><img src="https://yayoirestaurants.com/frontend/assets/images/icon/ins_icon.svg"></a></div>
+                <div><a href="https://www.youtube.com/user/YayoiThailand" target="_blank"><img src="https://yayoirestaurants.com/frontend/assets/images/icon/yt_icon.svg"></a></div>
+            </div>
+            
+        </div><br>
+        <div id="credit">
+            <div style="transform: translateY(10px);">
+                <img width="100%" src="https://yayoirestaurants.com/frontend/assets/images/bg_footer_bottom.png?c3d6b62">
+            </div>
+            <div id="creditD">© 2557-2563 บริษัท เอ็มเค อินเตอร์ฟู้ด</div>
+        </div>
+    </footer>
+
+
+<script type="text/javascript" src="https://yayoirestaurants.com/frontend/assets/dist/customize/text.th.js"></script>
+<script type="text/javascript" src="https://yayoirestaurants.com/frontend/assets/dist/app.js"></script>
+<script type="text/javascript" src="https://yayoirestaurants.com/frontend/assets/dist/customize/global.fn.js"></script>
+<script type="text/javascript" src="https://yayoirestaurants.com/frontend/assets/dist/customize/init.fn.js"></script>
+<script type="text/javascript" src="https://yayoirestaurants.com/frontend/assets/dist/customize/cart.js?v=1.2"></script>
+
 </body>
 </html>
